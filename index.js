8b7bf0b7698ee5b9f0c9b370f62cfcea67bf09c4
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2023-04-25 00:26:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const express = require("express");
 const app = express();
+const os = require('os');
 const multer = require("multer");
-const upload = multer({ dest: "uploads/"});
-const {PowerShell} = require("node-powershell")
+
+const { PowerShell } = require("node-powershell");
+const { randomUUID } = require("crypto");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `${os.tmpdir()}/`);
+  },
+  filename: function (req, file, cb) {
+    cb(null, randomUUID() + ".pdf");
+  },
+});
+
+const upload = multer({ storage });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 
 // .arg(format!(
 //     "Get-Content -Path \"{}\" |  Out-Printer -Name \"{}\"",
 //     temp_path.clone().display(), "Microsoft Print to PDF"
 // )).spawn();
 
-app.post("/print", upload.single("file"),async (req,res) => {
-    console.log(req.file)
-    console.log(await PowerShell.$`Get-Content -Path ${req.file.path} | Out-Printer -Name ${"Microsoft Print to PDF"}`)
-    return res.status(200).json("hi")
+app.post("/print", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  console.log(
+    await PowerShell.$`Get-Content -Path ${
+      req.file.path
+    } | Out-Printer -Name ${"Microsoft Print to PDF"}`
+  );
+  return res.status(200).json("hi");
 });
 
-
 app.listen(5000, () => {
-    console.log(`Server started...`);
+  console.log(`Server started...`);
 });
